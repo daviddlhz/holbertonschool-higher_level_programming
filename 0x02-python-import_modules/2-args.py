66e340501ad28699115d6ec796557889f373a1a7
daviddlhz <vidda1959@gmail.com>
2020-04-30 22:32:53 -0500

    Fix the problem

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,7 @@ import sys
 if __name__ == "__main__":
     if len(sys.argv) == 1:
         print("0 arguments.")
-    else if len(sys.argv) == 2:
+    elif len(sys.argv) == 2:
         print("1 argument:")
     else:
         print("{} arguments:".format(len(sys.argv) - 1))
