4a1b9833fb6b551eecb5ab44e346189f8cc7ffdb
daviddlhz <vidda1959@gmail.com>
2020-05-19 18:24:05 -0500

    Private atribute is found

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Class with atributte private"""
+
+
+class Square:
+    """Square class with initialization
+    """
+    def __init__(self, size):
+        """Init
+
+        Arguments:
+            size {[private]} -- [size of square]
+        """
+        self.__size = size
