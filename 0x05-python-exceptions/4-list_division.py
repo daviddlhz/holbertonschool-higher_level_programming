db367bde54b152ab1e3bde52c9a5291155bd95ec
daviddlhz <vidda1959@gmail.com>
2020-05-18 21:19:33 -0500

    Fix the error

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -4,7 +4,7 @@ def list_division(my_list_1, my_list_2, list_length):
     cnt = 0
     for i in range(list_length):
         try:
-            count = my_list_1[i] / my_list_2[i]
+            cnt = my_list_1[i] / my_list_2[i]
         except(ValueError, TypeError):
             print("wrong type")
             cnt = 0
