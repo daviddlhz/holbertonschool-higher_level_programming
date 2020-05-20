c55ec1b3a073062675532f43e4a06e22370b4518
daviddlhz <vidda1959@gmail.com>
2020-05-19 21:11:16 -0500

    Setters and getters

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Class with atributte private"""
+
+
+class Square:
+    """Square class with initialization
+    """
+    def __init__(self, size=0):
+        """Init
+
+        Arguments:
+            size {[private]} -- [size of square, value default = 0]
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        '''method get'''
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        '''method set'''
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """[Area of square]
+
+        Returns:
+            [size] -- [square a la dos]
+        """
+        return self.__size ** 2
