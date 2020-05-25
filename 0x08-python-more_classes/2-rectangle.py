4300a6bb0f880942e96fc9a28a5715f35fb26e5e
daviddlhz <vidda1959@gmail.com>
2020-05-25 18:29:08 -0500

    Area and perimeter of rectangle

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+"""Rectangule class
+"""
+
+
+class Rectangle:
+    """Initialization variables
+    """
+    def __init__(self, width=0, height=0):
+        self.__height = height
+        self.__width = width
+
+        """[Get and setters]
+
+        Atributtes:
+            private: width
+            private: heigth
+
+        Raises:
+            TypeError: [width must be an integer]
+            ValueError: [width must be >= 0]
+            TypeError: [height must be an integer]
+            ValueError: [width must be >= 0]
+
+        Returns:
+            [Getters] -- [Access all values of the variables or atributtes]
+            [area] -- [area of rectangle]
+            [perimeter] -- [perimeter of rectangle]
+        """
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        elif (width < 0):
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        elif (height < 0):
+            raise ValueError("width must be >= 0")
+        else:
+            self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        return 2 * (self.__width + self.__height)
