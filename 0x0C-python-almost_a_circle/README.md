3a96db7c31f06c89a44093f3b5192bd662a30a98
daviddlhz <vidda1959@gmail.com>
2020-06-05 23:55:10 -0500

    Initial commit

---
+++ 0x0C-python-almost_a_circle/README.md
@@ -0,0 +1,14 @@
+# holbertonschool-higher_level_programming :computer:
+
+## 0x0C-python-almost_a_circle :earth_asia:
+
+### files :page_facing_up:
+
+* models/base.py
+* models/square.py
+* models/rectangle.py
+* tests/
+
+## Author :black_nib:
+
+* **David De La Hoz** [Github](https://github.com/daviddlhz)
---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""[Class Base]
+"""
+
+
+class Base:
+    """[Initialization variables]
+    """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """[Method constructor]
+
+        Args:
+            id ([number]): [id of instances]. Defaults to None.
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
