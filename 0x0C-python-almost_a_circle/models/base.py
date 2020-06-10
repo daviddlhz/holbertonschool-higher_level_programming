83d628f00d29902d2ced3ceede863b623832acb1
daviddlhz <vidda1959@gmail.com>
2020-06-09 19:20:25 -0500

    Complete task

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -31,3 +31,56 @@ class Base:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        Method save json
+        """
+        lists = []
+        if len(list_objs) is not 0:
+            for i in list_objs:
+                lists.append(i.to_dictionary())
+        dicts = cls.to_json_string(lists)
+
+        with open(cls.__name__ + ".json", "w+") as my_file:
+            my_file.write(dicts)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """
+        returns the list of the JSON string representation json_string
+        """
+        if json_string is [None, ""]:
+            return "[]"
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """
+        returns an instance with all attributes already set
+        """
+        if cls.__name__ == "Rectangle":
+            tmp = cls(1, 1)
+            tmp.update(**dictionary)
+            return tmp
+        if cls.__name__ == "Square":
+            tmp = cls(1)
+            tmp.update(**dictionary)
+            return tmp
+
+    @classmethod
+    def load_from_file(cls):
+        """
+        Method returns a list of instances
+        """
+        try:
+            with open(cls.__name__ + ".json", "r") as my_file:
+                read = my_file.read()
+                lists = Base.from_json_string(read)
+                create = []
+                for i in lists:
+                    create.append(cls.create(**i))
+                return create
+        except IOError:
+            return []
